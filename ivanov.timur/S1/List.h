9252b5a8ed406a79fe73417c2dd671754075bd2b
isaacgreenfield <ivtim708@gmail.com>
2026-03-02 19:44:52 +0300

    initial commit

---
+++ ivanov.timur/S1/List.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+template <class T> class List;
+
+template <class T>
+class LIter {
+  friend class List<T>;
+
+}
+
+template <class T>
+class LCIter {
+  friend class List<T>;
+}
+
+template <class T>
+class List {
+
+}
+#endif
