6bd061037136536506bf99565b22f7f2d89679b4
isaacgreenfield <ivtim708@gmail.com>
2026-03-06 15:50:27 +0300

    forgot namespace

--- ivanov.timur/S1/List.h
+++ ivanov.timur/S1/List.h
@@ -3,282 +3,282 @@
 
 #include <stdexcept>
 #include <utility>
+namespace ivanov {
+    template <class T> class List;
 
-template <class T> class List;
+    template <class T>
+    class Iter {
+        friend class List<T>;
 
-template <class T>
-class Iter {
-    friend class List<T>;
-
-public:
-    Iter() noexcept : ptr(nullptr) {}
-    Iter(const Iter&) noexcept = default;
-    Iter(Iter&&) noexcept = default;
-    ~Iter() = default;
-    Iter& operator=(const Iter&) noexcept = default;
-    Iter& operator=(Iter&&) noexcept = default;
+    public:
+        Iter() noexcept : ptr(nullptr) {}
+        Iter(const Iter&) noexcept = default;
+        Iter(Iter&&) noexcept = default;
+        ~Iter() = default;
+        Iter& operator=(const Iter&) noexcept = default;
+        Iter& operator=(Iter&&) noexcept = default;
 
 
-    T& operator*() const noexcept {
-        return ptr->data_;
-    }
-    T* operator->() const noexcept {
-        return &(ptr->data_);
-    }
+        T& operator*() const noexcept {
+            return ptr->data_;
+        }
+        T* operator->() const noexcept {
+            return &(ptr->data_);
+        }
 
-    Iter& operator++() noexcept {
-        ptr = ptr->next_;
-        return *this;
-    }
+        Iter& operator++() noexcept {
+            ptr = ptr->next_;
+            return *this;
+        }
 
-    bool operator==(const Iter& other) const noexcept {
-        return ptr == other.ptr;
-    }
-    bool operator!=(const Iter& other) const noexcept {
-        return ptr != other.ptr;
-    }
+        bool operator==(const Iter& other) const noexcept {
+            return ptr == other.ptr;
+        }
+        bool operator!=(const Iter& other) const noexcept {
+            return ptr != other.ptr;
+        }
 
-private:
-    typename List<T>::Elem* ptr;
-    explicit Iter(typename List<T>::Elem* p) noexcept : ptr(p) {}
-};
-
-template <class T>
-class CIter {
-    friend class List<T>;
-
-public:
-    CIter() noexcept : ptr(nullptr) {}
-    CIter(const CIter&) noexcept = default;
-    CIter(CIter&&) noexcept = default;
-    ~CIter() = default;
-    CIter& operator=(const CIter&) noexcept = default;
-    CIter& operator=(CIter&&) noexcept = default;
-
-    const T& operator*() const noexcept {
-        return ptr->data_;
-    }
-    const T* operator->() const noexcept {
-        return &(ptr->data_);
-    }
-
-    CIter& operator++() noexcept {
-        ptr = ptr->next_;
-        return *this;
-    }
-
-    bool operator==(const CIter& other) const noexcept {
-        return ptr == other.ptr;
-    }
-    bool operator!=(const CIter& other) const noexcept {
-        return ptr != other.ptr;
-    }
-
-private:
-    const typename List<T>::Elem* ptr;
-    explicit CIter(const typename List<T>::Elem* p) noexcept : ptr(p) {}
-};
-
-template <typename T>
-class List {
-private:
-    struct Elem {
-        T data;
-        Elem* next;
-        explicit Elem(const T& val, Elem* nxt = nullptr) : data(val), next(nxt) {}
-        explicit Elem(T&& val, Elem* nxt = nullptr) : data(std::move(val)), next(nxt) {}
+    private:
+        typename List<T>::Elem* ptr;
+        explicit Iter(typename List<T>::Elem* p) noexcept : ptr(p) {}
     };
 
-    Elem* head;
-    Elem* tail;
-    size_t sz;
+    template <class T>
+    class CIter {
+        friend class List<T>;
 
-public:
-    List() noexcept : head(nullptr), tail(nullptr), sz(0) {}
+    public:
+        CIter() noexcept : ptr(nullptr) {}
+        CIter(const CIter&) noexcept = default;
+        CIter(CIter&&) noexcept = default;
+        ~CIter() = default;
+        CIter& operator=(const CIter&) noexcept = default;
+        CIter& operator=(CIter&&) noexcept = default;
 
-    ~List() {
-        clear();
-    }
-
-    List(const List& other) : head(nullptr), tail(nullptr), sz(0) {
-        for (Elem* curr = other.head; curr != nullptr; curr = curr->next) {
-            push_back(curr->data);
+        const T& operator*() const noexcept {
+            return ptr->data_;
         }
-    }
-
-    List(List&& other) noexcept : head(other.head), tail(other.tail), sz(other.sz) {
-        other.head = nullptr;
-        other.tail = nullptr;
-        other.sz = 0;
-    }
-
-    List& operator=(const List& other) {
-        if (this != &other) {
-            List tmp(other);
-            swap(tmp);
+        const T* operator->() const noexcept {
+            return &(ptr->data_);
         }
-        return *this;
-    }
 
-    List& operator=(List&& other) noexcept {
-        if (this != &other) {
+        CIter& operator++() noexcept {
+            ptr = ptr->next_;
+            return *this;
+        }
+
+        bool operator==(const CIter& other) const noexcept {
+            return ptr == other.ptr;
+        }
+        bool operator!=(const CIter& other) const noexcept {
+            return ptr != other.ptr;
+        }
+
+    private:
+        const typename List<T>::Elem* ptr;
+        explicit CIter(const typename List<T>::Elem* p) noexcept : ptr(p) {}
+    };
+
+    template <typename T>
+    class List {
+    private:
+        struct Elem {
+            T data;
+            Elem* next;
+            explicit Elem(const T& val, Elem* nxt = nullptr) : data(val), next(nxt) {}
+            explicit Elem(T&& val, Elem* nxt = nullptr) : data(std::move(val)), next(nxt) {}
+        };
+
+        Elem* head;
+        Elem* tail;
+        size_t sz;
+
+    public:
+        List() noexcept : head(nullptr), tail(nullptr), sz(0) {}
+
+        ~List() {
             clear();
-            head = other.head;
-            tail = other.tail;
-            sz   = other.sz;
+        }
+
+        List(const List& other) : head(nullptr), tail(nullptr), sz(0) {
+            for (Elem* curr = other.head; curr != nullptr; curr = curr->next) {
+                push_back(curr->data);
+            }
+        }
+
+        List(List&& other) noexcept : head(other.head), tail(other.tail), sz(other.sz) {
             other.head = nullptr;
             other.tail = nullptr;
             other.sz = 0;
         }
-        return *this;
-    }
 
-    void swap(List& other) noexcept {
-        Elem *tmp_head = head;
-        Elem *tmp_tail = tail;
-        size_t tmp_sz = sz;
-        head = other.head;
-        tail = other.tail;
-        sz = other.sz;
-        other.head = tmp_head;
-        other.tail = tmp_tail;
-        other.sz = tmp_sz;
-    }
-
-    Iter<T> begin() noexcept {
-        return Iter<T>(head);
-    };
-    Iter<T> end() noexcept {
-        return Iter<T>(nullptr);
-    };
-
-    CIter<T> cbegin() noexcept {
-        return CIter<T>(head);
-    }
-    CIter<T> cend() noexcept {
-        return CIter<T>(nullptr);
-    }
-
-    bool empty() const noexcept {
-        return sz == 0;
-    };
-    size_t size() const noexcept {
-        return sz;
-    };
-
-    T& front() {
-        return head->data;
-    };
-    const T& front() const {
-        return head->data;
-    };
-    T& back() {
-        return tail->data;
-    };
-    const T& back() const {
-        return tail->data;
-    };
-
-    void push_front(const T& value) {
-        Elem* nw = new Elem(value, head);
-        if (empty()) tail = nw;
-        head = nw;
-        sz++;
-    };
-    void push_front(T&& value) {
-        Elem* nw = new Elem(std::move(value), head);
-        if (empty()) tail = nw;
-        head = nw;
-        sz++;
-    };
-    void pop_front() {
-        if (head == tail) {
-            delete head;
-            head = nullptr;
-            tail = nullptr;
-        } else {
-            Elem* tmp = head->next;
-            delete head;
-            head = tmp;
+        List& operator=(const List& other) {
+            if (this != &other) {
+                List tmp(other);
+                swap(tmp);
+            }
+            return *this;
         }
-        sz--;
-    };
 
-    void push_back(const T& value) {
-        if (empty()) {
-            head = new Elem(value);
-            tail = head;
-        } else {
-            tail->next = new Elem(value);
-            tail = tail->next;
+        List& operator=(List&& other) noexcept {
+            if (this != &other) {
+                clear();
+                head = other.head;
+                tail = other.tail;
+                sz   = other.sz;
+                other.head = nullptr;
+                other.tail = nullptr;
+                other.sz = 0;
+            }
+            return *this;
         }
-        sz++;
-    };
-    void push_back(T&& value) {
-        if (empty()) {
-            head = new Elem(std::move(value));
-            tail = head;
-        } else {
-            tail->next = new Elem(std::move(value));
-            tail = tail->next;
-        }
-        sz++;
-    };
-    void pop_back() {
-        if (head == tail) {
-            delete head;
-            head = nullptr;
-            tail = nullptr;
-        } else {
-            Elem* tmp = head;
-            while (tmp->next != tail) tmp = tmp->next;
-            tmp->next = nullptr;
-            delete tail;
-            tail = tmp;
-        }
-        sz--;
-    };
 
-    Iter<T> insert_after(Iter<T> pos, const T& value) {
-        Elem* curr = pos.ptr;
-        if (curr == nullptr)
-            throw std::out_of_range("cannot insert in nullptr");
-        Elem* nw = new Elem(value, curr->next);
-        curr->next = nw;
-        if (curr == tail) {
-            tail = nw;
+        void swap(List& other) noexcept {
+            Elem *tmp_head = head;
+            Elem *tmp_tail = tail;
+            size_t tmp_sz = sz;
+            head = other.head;
+            tail = other.tail;
+            sz = other.sz;
+            other.head = tmp_head;
+            other.tail = tmp_tail;
+            other.sz = tmp_sz;
         }
-        sz++;
-        return Iter<T>(nw);
-    };
-    Iter<T> insert_after(Iter<T> pos, T&& value) {
-        Elem* curr = pos.ptr;
-        if (curr == nullptr)
-            throw std::out_of_range("cannot insert in nullptr");
-        Elem* nw = new Elem(std::move(value), curr->next);
-        curr->next = nw;
-        if (curr == tail) {
-            tail = nw;
+
+        Iter<T> begin() noexcept {
+            return Iter<T>(head);
+        };
+        Iter<T> end() noexcept {
+            return Iter<T>(nullptr);
+        };
+
+        CIter<T> cbegin() noexcept {
+            return CIter<T>(head);
         }
-        sz++;
-        return Iter<T>(nw);
-    };
+        CIter<T> cend() noexcept {
+            return CIter<T>(nullptr);
+        }
 
-    Iter<T> erase_after(Iter<T> pos) {
-        Elem* curr = pos.ptr;
-        if (curr == nullptr || curr->next == nullptr)
-            throw std::out_of_range("cannot erase from nullptr");
-        Elem* tmp = curr->next->next;
-        if (curr->next == tail) tail = curr;
-        delete curr->next;
-        curr->next = tmp;
-        sz--;
-        return Iter<T>(curr->next);
-    };
+        bool empty() const noexcept {
+            return sz == 0;
+        };
+        size_t size() const noexcept {
+            return sz;
+        };
 
-    void clear() {
-        while (head != nullptr) pop_front();
-    };
-};
+        T& front() {
+            return head->data;
+        };
+        const T& front() const {
+            return head->data;
+        };
+        T& back() {
+            return tail->data;
+        };
+        const T& back() const {
+            return tail->data;
+        };
 
+        void push_front(const T& value) {
+            Elem* nw = new Elem(value, head);
+            if (empty()) tail = nw;
+            head = nw;
+            sz++;
+        };
+        void push_front(T&& value) {
+            Elem* nw = new Elem(std::move(value), head);
+            if (empty()) tail = nw;
+            head = nw;
+            sz++;
+        };
+        void pop_front() {
+            if (head == tail) {
+                delete head;
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                Elem* tmp = head->next;
+                delete head;
+                head = tmp;
+            }
+            sz--;
+        };
+
+        void push_back(const T& value) {
+            if (empty()) {
+                head = new Elem(value);
+                tail = head;
+            } else {
+                tail->next = new Elem(value);
+                tail = tail->next;
+            }
+            sz++;
+        };
+        void push_back(T&& value) {
+            if (empty()) {
+                head = new Elem(std::move(value));
+                tail = head;
+            } else {
+                tail->next = new Elem(std::move(value));
+                tail = tail->next;
+            }
+            sz++;
+        };
+        void pop_back() {
+            if (head == tail) {
+                delete head;
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                Elem* tmp = head;
+                while (tmp->next != tail) tmp = tmp->next;
+                tmp->next = nullptr;
+                delete tail;
+                tail = tmp;
+            }
+            sz--;
+        };
+
+        Iter<T> insert_after(Iter<T> pos, const T& value) {
+            Elem* curr = pos.ptr;
+            if (curr == nullptr)
+                throw std::out_of_range("cannot insert in nullptr");
+            Elem* nw = new Elem(value, curr->next);
+            curr->next = nw;
+            if (curr == tail) {
+                tail = nw;
+            }
+            sz++;
+            return Iter<T>(nw);
+        };
+        Iter<T> insert_after(Iter<T> pos, T&& value) {
+            Elem* curr = pos.ptr;
+            if (curr == nullptr)
+                throw std::out_of_range("cannot insert in nullptr");
+            Elem* nw = new Elem(std::move(value), curr->next);
+            curr->next = nw;
+            if (curr == tail) {
+                tail = nw;
+            }
+            sz++;
+            return Iter<T>(nw);
+        };
+
+        Iter<T> erase_after(Iter<T> pos) {
+            Elem* curr = pos.ptr;
+            if (curr == nullptr || curr->next == nullptr)
+                throw std::out_of_range("cannot erase from nullptr");
+            Elem* tmp = curr->next->next;
+            if (curr->next == tail) tail = curr;
+            delete curr->next;
+            curr->next = tmp;
+            sz--;
+            return Iter<T>(curr->next);
+        };
+
+        void clear() {
+            while (head != nullptr) pop_front();
+        };
+    };
+}
 #endif
