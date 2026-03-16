f6deb5e954f29f591dd42e7b51477a62c3fb1fea
isaacgreenfield <ivtim708@gmail.com>
2026-03-16 08:59:36 +0300

    Initial commit + created Stack/Queue

---
+++ ivanov.timur/S2/Queue.h
@@ -0,0 +1,44 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include "List.h"
+
+template <typename T>
+class Queue {
+  ivanov::List<T>* head;
+public:
+  Queue(ivanov::List<T>* nwh);
+  ~Queue() = default;
+  void push (T rhs);
+  T drop();
+  bool isEmpty();
+};
+
+template<typename T>
+bool Queue<T>::isEmpty() {
+  return head->empty();
+}
+
+template<typename T>
+Queue<T>::Queue(ivanov::List<T> *nwh): head(nwh) {}
+
+template<typename T>
+T Queue<T>::drop() {
+  if (!isEmpty()) {
+    T ans = head->front();
+    head->pop_front();
+    return ans;
+  }
+  throw std::logic_error("no elems");
+}
+
+template<typename T>
+void Queue<T>::push(T rhs) {
+  if (!isEmpty) head->push_back(rhs);
+  else {
+    head = new ivanov::List<T>();
+    head->push_back(rhs);
+    head->pop_front();
+  }
+}
+
+#endif //QUEUE_H
