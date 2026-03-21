04f0cc54cec096f727e267546d8d87c067491dc2
isaacgreenfield <ivtim708@gmail.com>
2026-03-21 20:08:54 +0300

    Made Object-List eval made namespace ivanov great again

--- ivanov.timur/S2/main.cpp
+++ ivanov.timur/S2/main.cpp
@@ -8,7 +8,7 @@
 #include "Stack.h"
 #include "List.h"
 
-int getPrecedence(char op) {
+int getP(char op) {
   switch (op) {
     case '+':
     case '-': return 1;
@@ -26,7 +26,7 @@ bool ira(char op) {
 
 ivanov::List<Object*> infixToPostfix(const ivanov::List<Object*>& infix) {
   ivanov::List<Object*> stackList;
-  Stack<Object*> stack(&stackList);
+  ivanov::Stack<Object*> stack(&stackList);
 
   ivanov::List<Object*> queueList;
   ivanov::Queue<Object*> output(&queueList);
@@ -47,11 +47,11 @@ ivanov::List<Object*> infixToPostfix(const ivanov::List<Object*>& infix) {
       }
     } else if (obj->symbol == '+' || obj->symbol == '-' || obj->symbol == '*' ||
                obj->symbol == '/' || obj->symbol == '%' || obj->symbol == '^') {
-      int prec = getPrecedence(obj->symbol);
+      int prec = getP(obj->symbol);
       bool rightAssoc = ira(obj->symbol);
       while (!stack.isEmpty() && stack.top()->symbol != '(') {
         char topSym = stack.top()->symbol;
-        int topPrec = getPrecedence(topSym);
+        int topPrec = getP(topSym);
         if ((rightAssoc && prec < topPrec) || (!rightAssoc && prec <= topPrec)) {
           output.push(stack.drop());
         } else {
@@ -90,6 +90,60 @@ ivanov::List<Object*> stringToInfixList(const std::string& expr) {
     }
   }
   return result;
+}
+
+Integer* eval(const ivanov::List<Object*>& line) {
+  ivanov::List<Object*> stackList;
+  ivanov::Stack<Object*> stack(&stackList);
+
+  for (Object* obj : line) {
+    if (obj->isNumber()) {
+      stack.push(obj);
+    } else {
+      Integer* right = static_cast<Integer*>(stack.drop());
+      Integer* left  = static_cast<Integer*>(stack.drop());
+      int resVal = 0;
+      switch (obj->symbol) {
+        case '+':
+          resVal = left->getValue() + right->getValue();
+          break;
+        case '-':
+          resVal = left->getValue() - right->getValue();
+          break;
+        case '*':
+          resVal = left->getValue() * right->getValue();
+          break;
+        case '/':
+          if (right->getValue() == 0) throw std::runtime_error("Division by zero");
+          resVal = left->getValue() / right->getValue();
+          break;
+        case '%':
+          if (right->getValue() == 0) throw std::runtime_error("Modulo by zero");
+          resVal = left->getValue() % right->getValue();
+          break;
+        case '^':
+        {
+          int base = left->getValue();
+          int exp = right->getValue();
+          if (exp < 0) throw std::runtime_error("Negative exponent not supported");
+          int powVal = 1;
+          for (int i = 0; i < exp; ++i) {
+            powVal *= base;
+          }
+          resVal = powVal;
+        }
+          break;
+        default:
+          throw std::invalid_argument("Unknown operator");
+      }
+      Integer* result = new Integer(resVal);
+      stack.push(result);
+    }
+  }
+
+  if (stack.isEmpty()) throw std::runtime_error("No result");
+  Integer* finalResult = static_cast<Integer*>(stack.drop());
+  return finalResult;
 }
 
 int main(int argc, char* argv[]) {
