89c21850ad997a636cdba0f1901f2ab04b5c44dc
isaacgreenfield <ivtim708@gmail.com>
2026-05-04 09:57:45 +0300

    Implemented iterator and const_iterator

--- ivanov.timur/S3/HashTable.h
+++ ivanov.timur/S3/HashTable.h
@@ -4,97 +4,96 @@
 #include <functional>
 #include <stdexcept>
 #include <vector>
+namespace ivanov {
+  template<class Key, class Value, class Hash, class Equal>
+  class HashTable {
+    struct Slot {
+      Key key;
+      Value value;
+      int state = 0; // 0 – пусто, 1 – занято, 2 – tombstone
+    };
 
-template<class Key, class Value, class Hash, class Equal>
-class HashTable {
-  struct Slot {
-    Key key;
-    Value value;
-    int state = 0; // 0 – пусто, 1 – занято, 2 – tombstone
-  };
+    std::vector<Slot> table;
+    size_t count = 0;
+    Hash hash;
+    Equal equal;
 
-  std::vector<Slot> table;
-  size_t count = 0;
-  Hash hash;
-  Equal equal;
-
-  size_t probe(size_t hash_f, size_t i) const noexcept {
-    return (hash_f + i * i) % table.size();
-  }
-
-  size_t find_slot(const Key &key) const;
-
-  size_t find_insert_slot(const Key &key);
-
-public:
-  void add(const Key &key, const Value &value);
-
-  bool has(const Key &key) const;
-
-  Value drop(const Key &key);
-
-  void rehash(size_t new_cap);
-
-  size_t size() const noexcept { return count; }
-  size_t capacity() const noexcept { return table.size(); }
-  bool empty() const noexcept { return count == 0; }
-
-  void clear() noexcept;
-
-  class iterator {
-    Slot *ptr;
-    Slot *end;
-
-    void skip_empty() {
-      while (ptr != end && ptr->state != 1) ++ptr;
+    size_t probe(size_t hash_f, size_t i) const noexcept {
+      return (hash_f + i * i) % table.size();
     }
 
-  public:
-    iterator(Slot *p, Slot *e) : ptr(p), end(e) { skip_empty(); }
+    size_t find_slot(const Key &key) const;
 
-    std::pair<const Key &, Value &> operator*() const;
-
-    iterator &operator++();
-
-    bool operator==(const iterator &other) const;
-
-    bool operator!=(const iterator &other) const;
-  };
-
-  class const_iterator {
-    const Slot *ptr;
-    const Slot *end;
-
-    void skip_empty() {
-      while (ptr != end && ptr->state != 1) ++ptr;
-    }
+    size_t find_insert_slot(const Key &key);
 
   public:
-    const_iterator(const Slot *p, const Slot *e) : ptr(p), end(e) { skip_empty(); }
+    void add(const Key &key, const Value &value);
 
-    std::pair<const Key &, const Value &> operator*() const;
+    bool has(const Key &key) const;
 
-    const_iterator &operator++();
+    Value drop(const Key &key);
 
-    bool operator==(const const_iterator &other) const;
+    void rehash(size_t new_cap);
 
-    bool operator!=(const const_iterator &other) const;
+    size_t size() const noexcept { return count; }
+    size_t capacity() const noexcept { return table.size(); }
+    bool empty() const noexcept { return count == 0; }
+
+    void clear() noexcept;
+
+    class iterator {
+      Slot *ptr;
+      Slot *end;
+
+      void skip_empty() {
+        while (ptr != end && ptr->state != 1) ++ptr;
+      }
+
+    public:
+      iterator(Slot *p, Slot *e) : ptr(p), end(e) { skip_empty(); }
+
+      std::pair<const Key &, Value &> operator*() const;
+
+      iterator &operator++();
+
+      bool operator==(const iterator &other) const {return ptr == other.ptr; };
+
+      bool operator!=(const iterator &other) const {return ptr != other.ptr; };
+    };
+
+    class const_iterator {
+      const Slot *ptr;
+      const Slot *end;
+
+      void skip_empty() {
+        while (ptr != end && ptr->state != 1) ++ptr;
+      }
+
+    public:
+      const_iterator(const Slot *p, const Slot *e) : ptr(p), end(e) { skip_empty(); }
+
+      std::pair<const Key &, const Value &> operator*() const;
+
+      const_iterator &operator++();
+
+      bool operator==(const const_iterator &other) const {return ptr == other.ptr; };
+
+      bool operator!=(const const_iterator &other) const {return ptr != other.ptr; };
+    };
+
+    iterator begin();
+    iterator end();
+
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+
+    HashTable();
+    HashTable(const HashTable &other);
+    ~HashTable() = default;
   };
+}
 
-  iterator begin();
-
-  iterator end();
-
-  const_iterator begin() const;
-
-  const_iterator end() const;
-
-  HashTable();
-
-  HashTable(const HashTable &other);
-
-  ~HashTable() = default;
-};
+using ivanov::HashTable;
 
 template<class Key, class Value, class Hash, class Equal>
 size_t HashTable<Key, Value, Hash, Equal>::find_slot(const Key &key) const {
@@ -190,4 +189,45 @@ void HashTable<Key, Value, Hash, Equal>::clear() noexcept {
   count = 0;
 }
 
+template<class Key, class Value, class Hash, class Equal>
+std::pair<const Key &, Value &> HashTable<Key, Value, Hash, Equal>::iterator::operator*() const {
+  return std::make_pair(ptr->key, ptr->value);
+}
+
+template<class Key, class Value, class Hash, class Equal>
+typename HashTable<Key, Value, Hash, Equal>::iterator & HashTable<Key, Value, Hash, Equal>::iterator::operator++() {
+  ++ptr;
+  skip_empty();
+  return *this;
+}
+
+template<class Key, class Value, class Hash, class Equal>
+std::pair<const Key &, const Value &> HashTable<Key, Value, Hash, Equal>::const_iterator::operator*() const {
+  return std::make_pair(ptr->key, ptr->value);
+}
+
+template<class Key, class Value, class Hash, class Equal>
+typename HashTable<Key, Value, Hash, Equal>::const_iterator & HashTable<Key, Value, Hash, Equal>::const_iterator::operator++() {
+  ++ptr;
+  skip_empty();
+  return *this;
+}
+
+template<class Key, class Value, class Hash, class Equal>
+typename HashTable<Key, Value, Hash, Equal>::iterator HashTable<Key, Value, Hash, Equal>::begin() {
+  return iterator(table.data(), table.data() + table.size());
+}
+template<class Key, class Value, class Hash, class Equal>
+typename HashTable<Key, Value, Hash, Equal>::iterator HashTable<Key, Value, Hash, Equal>::end() {
+  return iterator(table.data() + table.size(), table.data() + table.size());
+}
+
+template<class Key, class Value, class Hash, class Equal>
+typename HashTable<Key, Value, Hash, Equal>::const_iterator HashTable<Key, Value, Hash, Equal>::cbegin() const {
+  return const_iterator(table.data(), table.data() + table.size());
+}
+template<class Key, class Value, class Hash, class Equal>
+typename HashTable<Key, Value, Hash, Equal>::const_iterator HashTable<Key, Value, Hash, Equal>::cend() const {
+  return const_iterator(table.data() + table.size(), table.data() + table.size());
+}
 #endif
