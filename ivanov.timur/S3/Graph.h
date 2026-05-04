f06f0c35d97966e27350db0c5f8808beb8b4143f
isaacgreenfield <ivtim708@gmail.com>
2026-05-04 11:06:06 +0300

    Implemented BOOST + minor polish

--- ivanov.timur/S3/Graph.h
+++ ivanov.timur/S3/Graph.h
@@ -1,23 +1,35 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <cstring>
 #include <vector>
 #include <functional>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/blake2b.hpp>
 #include "HashTable.h"
 
 namespace ivanov {
   template<class Key>
   struct Edge {
     Key from, to;
-    bool operator==(const Edge& other) const noexcept {
+
+    bool operator==(const Edge &other) const noexcept {
       return from == other.from && to == other.to;
     }
   };
 
   template<class Key>
   struct EdgeHash {
-    size_t operator()(const Edge<Key>& e) const noexcept {
-      return 1;
+    size_t operator()(const Edge<Key> &e) const noexcept {
+      boost::hash2::blake2b_512 hasher;
+
+      boost::hash2::hash_append(hasher, e.from);
+      boost::hash2::hash_append(hasher, e.to);
+
+      auto digest = hasher.result();
+      size_t result = 0;
+      std::memcpy(&result, digest.data(), sizeof(result));
+      return result;
     }
   };
 
@@ -25,18 +37,21 @@ namespace ivanov {
   class Graph {
   public:
     std::vector<Key> vertices;
-    HashTable<Edge<Key>,std::vector<Weight>, EdgeHash<Key>,std::equal_to<Edge<Key>>> edges;
+    HashTable<Edge<Key>, std::vector<Weight>, EdgeHash<Key>, std::equal_to<Edge<Key> > > edges;
 
-    void createFrom(const std::vector<Key>& list) {
+    void createFrom(const std::vector<Key> &list) {
       vertices = list;
     }
-    void addVertex(const Key& name) {
+
+    void addVertex(const Key &name) {
       vertices.push_back(name);
     }
-    bool removeVertex(const Key& name);
 
-    void addEdge(const Key& from, const Key& to, Weight w);
-    bool removeEdge(const Key& from, const Key& to, Weight w);
+    bool removeVertex(const Key &name);
+
+    void addEdge(const Key &from, const Key &to, Weight w);
+
+    bool removeEdge(const Key &from, const Key &to, Weight w);
   };
 
   template<class Key, class Weight>
@@ -44,14 +59,14 @@ namespace ivanov {
     auto it = std::find(vertices.begin(), vertices.end(), name);
     if (it == vertices.end()) return false;
 
-    std::vector<Edge<Key>> toDelete;
+    std::vector<Edge<Key> > toDelete;
     for (auto edgeIt = edges.begin(); edgeIt != edges.end(); ++edgeIt) {
-      const auto& edge = (*edgeIt).first;
+      const auto &edge = (*edgeIt).first;
       if (edge.from == name || edge.to == name) {
         toDelete.push_back(edge);
       }
     }
-    for (const auto& e : toDelete) {
+    for (const auto &e: toDelete) {
       if (edges.has(e)) {
         edges.drop(e);
       }
@@ -65,7 +80,7 @@ namespace ivanov {
     Edge<Key> key{from, to};
     if (edges.has(key)) {
       for (auto it = edges.begin(); it != edges.end(); ++it) {
-        auto& pair = *it;
+        auto &pair = *it;
         if (pair.first == key) {
           pair.second.push_back(w);
           return;
@@ -81,9 +96,9 @@ namespace ivanov {
     Edge<Key> key{from, to};
     if (!edges.has(key)) return false;
     for (auto it = edges.begin(); it != edges.end(); ++it) {
-      auto& pair = *it;
+      auto &pair = *it;
       if (pair.first == key) {
-        auto& weights = pair.second;
+        auto &weights = pair.second;
         auto wIt = std::find(weights.begin(), weights.end(), w);
         if (wIt == weights.end()) return false;
         weights.erase(wIt);
