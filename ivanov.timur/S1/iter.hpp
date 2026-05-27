2f80711e26ec53fe6b2106c0ee6178ae3e3b8e7a
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2026-05-27 23:55:28 +0300

    Iter CG fix

--- ivanov.timur/S1/iter.hpp
+++ ivanov.timur/S1/iter.hpp
@@ -2,98 +2,123 @@
 #define ITER_HPP
 
 #include <iterator>
+
 #include "list.hpp"
 
-namespace ivanov {
+namespace ivanov
+{
   template < class T >
-  class Iter : public std::iterator<std::forward_iterator_tag, T> {
+  class Iter : public std::iterator< std::forward_iterator_tag, T >
+  {
     friend class List< T >;
 
   public:
     Iter() noexcept
-    : ptr(nullptr) {
+      : ptr_(nullptr)
+    {
     }
+
     Iter(const Iter&) noexcept = default;
     Iter(Iter&&) noexcept = default;
     ~Iter() = default;
+
     Iter& operator=(const Iter&) noexcept = default;
     Iter& operator=(Iter&&) noexcept = default;
 
-    T& operator*() const noexcept {
-      return ptr->data;
-    }
-    T* operator->() const noexcept {
-      return &(ptr->data);
+    T& operator*() const noexcept
+    {
+      return ptr_->data_;
     }
 
-    Iter& operator++() noexcept {
-      ptr = ptr->next;
+    T* operator->() const noexcept
+    {
+      return &(ptr_->data_);
+    }
+
+    Iter& operator++() noexcept
+    {
+      ptr_ = ptr_->next_;
       return *this;
     }
 
-    bool operator==(const Iter& other) const noexcept {
-      return ptr == other.ptr;
+    bool operator==(const Iter& other) const noexcept
+    {
+      return ptr_ == other.ptr_;
     }
-    bool operator!=(const Iter& other) const noexcept {
-      return ptr != other.ptr;
+
+    bool operator!=(const Iter& other) const noexcept
+    {
+      return ptr_ != other.ptr_;
     }
 
   private:
-    typename List< T >::Elem* ptr;
+    typename List< T >::Elem* ptr_;
+
     explicit Iter(typename List< T >::Elem* p) noexcept
-    : ptr(p) {
+      : ptr_(p)
+    {
     }
   };
 
   template < class T >
-  Iter< T > List< T >::begin() const noexcept {
-    return Iter< T >(head);
+  Iter< T > List< T >::begin() const noexcept
+  {
+    return Iter< T >(head_);
   }
+
   template < class T >
-  Iter< T > List< T >::end() const noexcept {
+  Iter< T > List< T >::end() const noexcept
+  {
     return Iter< T >(nullptr);
   }
 
   template < class T >
-  Iter< T > List< T >::insert_after(Iter< T > pos, const T& value) {
-    Elem* curr = pos.ptr;
+  Iter< T > List< T >::insert_after(const Iter< T > pos, const T& value)
+  {
+    Elem* curr = pos.ptr_;
     if (curr == nullptr) {
       throw std::out_of_range("cannot insert in nullptr");
     }
-    Elem* nw = new Elem(value, curr->next);
-    curr->next = nw;
-    if (curr == tail) {
-      tail = nw;
+    Elem* nw = new Elem(value, curr->next_);
+    curr->next_ = nw;
+    if (curr == tail_) {
+      tail_ = nw;
     }
-    sz++;
+    sz_++;
     return Iter< T >(nw);
   }
+
   template < class T >
-  Iter< T > List< T >::insert_after(Iter< T > pos, T&& value) {
-    Elem* curr = pos.ptr;
+  Iter< T > List< T >::insert_after(const Iter< T > pos, T&& value)
+  {
+    Elem* curr = pos.ptr_;
     if (curr == nullptr) {
       throw std::out_of_range("cannot insert in nullptr");
     }
-    Elem* nw = new Elem(std::move(value), curr->next);
-    curr->next = nw;
-    if (curr == tail) {
-      tail = nw;
+    Elem* nw = new Elem(std::move(value), curr->next_);
+    curr->next_ = nw;
+    if (curr == tail_) {
+      tail_ = nw;
     }
-    sz++;
+    sz_++;
     return Iter< T >(nw);
   }
+
   template < class T >
-  Iter< T > List< T >::erase_after(Iter< T > pos) {
-    Elem* curr = pos.ptr;
-    if (curr == nullptr || curr->next == nullptr) {
+  Iter< T > List< T >::erase_after(const Iter< T > pos)
+  {
+    Elem* curr = pos.ptr_;
+    if (curr == nullptr || curr->next_ == nullptr) {
       throw std::out_of_range("cannot erase from nullptr");
     }
-    Elem* tmp = curr->next->next;
-    if (curr->next == tail) tail = curr;
-    delete curr->next;
-    curr->next = tmp;
-    sz--;
-    return Iter< T >(curr->next);
+    Elem* tmp = curr->next_->next_;
+    if (curr->next_ == tail_) {
+      tail_ = curr;
+    }
+    delete curr->next_;
+    curr->next_ = tmp;
+    sz_--;
+    return Iter< T >(curr->next_);
   }
 }
 
