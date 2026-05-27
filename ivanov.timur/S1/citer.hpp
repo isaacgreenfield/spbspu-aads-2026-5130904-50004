13fb216746834b5c6e371a83856f5b385789fec8
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2026-05-27 23:57:00 +0300

    Citer CG fix

--- ivanov.timur/S1/citer.hpp
+++ ivanov.timur/S1/citer.hpp
@@ -2,55 +2,73 @@
 #define CITER_HPP
 
 #include <iterator>
+
 #include "list.hpp"
 
-namespace ivanov {
+namespace ivanov
+{
   template < class T >
-  class CIter : public std::iterator<std::forward_iterator_tag, T, std::ptrdiff_t, const T*, const T&> {
+  class CIter : public std::iterator< std::forward_iterator_tag, T, std::ptrdiff_t, const T*, const T& >
+  {
     friend class List< T >;
 
   public:
     CIter() noexcept
-    : ptr(nullptr) {
+      : ptr_(nullptr)
+    {
     }
+
     CIter(const CIter&) noexcept = default;
     CIter(CIter&&) noexcept = default;
     ~CIter() = default;
+
     CIter& operator=(const CIter&) noexcept = default;
     CIter& operator=(CIter&&) noexcept = default;
 
-    const T& operator*() const noexcept {
-      return ptr->data;
-    }
-    const T* operator->() const noexcept {
-      return &(ptr->data);
+    const T& operator*() const noexcept
+    {
+      return ptr_->data_;
     }
 
-    CIter& operator++() noexcept {
-      ptr = ptr->next;
+    const T* operator->() const noexcept
+    {
+      return &(ptr_->data_);
+    }
+
+    CIter& operator++() noexcept
+    {
+      ptr_ = ptr_->next_;
       return *this;
     }
 
-    bool operator==(const CIter& other) const noexcept {
-      return ptr == other.ptr;
+    bool operator==(const CIter& other) const noexcept
+    {
+      return ptr_ == other.ptr_;
     }
-    bool operator!=(const CIter& other) const noexcept {
-      return ptr != other.ptr;
+
+    bool operator!=(const CIter& other) const noexcept
+    {
+      return ptr_ != other.ptr_;
     }
 
   private:
-    const typename List< T >::Elem* ptr;
+    const typename List< T >::Elem* ptr_;
+
     explicit CIter(const typename List< T >::Elem* p) noexcept
-    : ptr(p) {
+      : ptr_(p)
+    {
     }
   };
 
   template < class T >
-  CIter< T > List< T >::cbegin() const noexcept {
-    return CIter< T >(head);
+  CIter< T > List< T >::cbegin() const noexcept
+  {
+    return CIter< T >(head_);
   }
+
   template < class T >
-  CIter< T > List< T >::cend() const noexcept {
+  CIter< T > List< T >::cend() const noexcept
+  {
     return CIter< T >(nullptr);
   }
 }
