414167be77b218897a3eec1485cba5b1a03c7625
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2026-05-27 23:48:23 +0300

    List CG fix

--- ivanov.timur/S1/list.hpp
+++ ivanov.timur/S1/list.hpp
@@ -5,56 +5,77 @@
 #include <utility>
 #include <limits>
 
-namespace ivanov {
+namespace ivanov
+{
   template < class T >
   class Iter;
   template < class T >
   class CIter;
 
   template < class T >
-  class List {
+  class List
+  {
     friend class Iter< T >;
     friend class CIter< T >;
+
   protected:
-    struct Elem {
-      T data;
-      Elem* next;
+    struct Elem
+    {
+      T data_;
+      Elem* next_;
+
       explicit Elem(const T& val, Elem* nxt = nullptr)
-      : data(val), next(nxt) {
+        : data_(val),
+          next_(nxt)
+      {
       }
 
       explicit Elem(T&& val, Elem* nxt = nullptr)
-      : data(std::move(val)), next(nxt) {
+        : data_(std::move(val)),
+          next_(nxt)
+      {
       }
     };
 
-    Elem* head;
-    Elem* tail;
-    size_t sz;
+    Elem* head_;
+    Elem* tail_;
+    size_t sz_;
 
   public:
     List() noexcept
-    : head(nullptr), tail(nullptr), sz(0) {
+      : head_(nullptr),
+        tail_(nullptr),
+        sz_(0)
+    {
     }
 
-    ~List() {
+    ~List()
+    {
       clear();
     }
 
     List(const List& other)
-    : head(nullptr), tail(nullptr), sz(0) {
-      for (Elem* curr = other.head; curr != nullptr; curr = curr->next) {
-        push_back(curr->data);
+      : head_(nullptr),
+        tail_(nullptr),
+        sz_(0)
+    {
+      for (Elem* curr = other.head_; curr != nullptr; curr = curr->next_) {
+        push_back(curr->data_);
       }
     }
 
-    List(List&& other) noexcept : head(other.head), tail(other.tail), sz(other.sz) {
-      other.head = nullptr;
-      other.tail = nullptr;
-      other.sz = 0;
+    List(List&& other) noexcept
+      : head_(other.head_),
+        tail_(other.tail_),
+        sz_(other.sz_)
+    {
+      other.head_ = nullptr;
+      other.tail_ = nullptr;
+      other.sz_ = 0;
     }
 
-    List& operator=(const List& other) {
+    List& operator=(const List& other)
+    {
       if (this != &other) {
         List tmp(other);
         swap(tmp);
@@ -62,23 +83,25 @@ namespace ivanov {
       return *this;
     }
 
-    List& operator=(List&& other) noexcept {
+    List& operator=(List&& other) noexcept
+    {
       if (this != &other) {
         clear();
-        head = other.head;
-        tail = other.tail;
-        sz   = other.sz;
-        other.head = nullptr;
-        other.tail = nullptr;
-        other.sz = 0;
+        head_ = other.head_;
+        tail_ = other.tail_;
+        sz_ = other.sz_;
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.sz_ = 0;
       }
       return *this;
     }
 
-    void swap(List& other) noexcept {
-      std::swap(head, other.head);
-      std::swap(tail, other.tail);
-      std::swap(sz, other.sz);
+    void swap(List& other) noexcept
+    {
+      std::swap(head_, other.head_);
+      std::swap(tail_, other.tail_);
+      std::swap(sz_, other.sz_);
     }
 
     Iter< T > begin() const noexcept;
@@ -86,115 +109,133 @@ namespace ivanov {
     CIter< T > cbegin() const noexcept;
     CIter< T > cend() const noexcept;
 
-    bool empty() const noexcept {
-      return sz == 0;
-    };
+    bool empty() const noexcept
+    {
+      return sz_ == 0;
+    }
 
-    size_t size() const noexcept {
-      return sz;
-    };
+    size_t size() const noexcept
+    {
+      return sz_;
+    }
 
-    T& front() {
-      return head->data;
-    };
+    T& front()
+    {
+      return head_->data_;
+    }
 
-    const T& front() const {
-      return head->data;
-    };
+    const T& front() const
+    {
+      return head_->data_;
+    }
 
-    T& back() {
-      return tail->data;
-    };
+    T& back()
+    {
+      return tail_->data_;
+    }
 
-    const T& back() const {
-      return tail->data;
-    };
+    const T& back() const
+    {
+      return tail_->data_;
+    }
 
-    void push_front(const T& value) {
-      Elem* nw = new Elem(value, head);
+    void push_front(const T& value)
+    {
+      Elem* nw = new Elem(value, head_);
       if (empty()) {
-        tail = nw;
+        tail_ = nw;
       }
-      head = nw;
-      sz++;
-    };
+      head_ = nw;
+      sz_++;
+    }
 
-    void push_front(T&& value) {
-      Elem* nw = new Elem(std::move(value), head);
+    void push_front(T&& value)
+    {
+      Elem* nw = new Elem(std::move(value), head_);
       if (empty()) {
-        tail = nw;
+        tail_ = nw;
       }
-      head = nw;
-      sz++;
-    };
+      head_ = nw;
+      sz_++;
+    }
 
-    void pop_front() {
-      if (empty()) {
-        return;
-      }
-      if (head == tail) {
-        delete head;
-        head = nullptr;
-        tail = nullptr;
-      } else {
-        Elem* tmp = head->next;
-        delete head;
-        head = tmp;
-      }
-      sz--;
-    };
-
-    void push_back(const T& value) {
-      if (empty()) {
-        head = new Elem(value);
-        tail = head;
-      } else {
-        tail->next = new Elem(value);
-        tail = tail->next;
-      }
-      sz++;
-    };
-
-    void push_back(T&& value) {
-      if (empty()) {
-        head = new Elem(std::move(value));
-        tail = head;
-      } else {
-        tail->next = new Elem(std::move(value));
-        tail = tail->next;
-      }
-      sz++;
-    };
-
-    void pop_back() {
+    void pop_front()
+    {
       if (empty()) {
         return;
       }
-      if (head == tail) {
-        delete head;
-        head = nullptr;
-        tail = nullptr;
+      if (head_ == tail_) {
+        delete head_;
+        head_ = nullptr;
+        tail_ = nullptr;
       } else {
-        Elem* tmp = head;
-        while (tmp->next != tail) tmp = tmp->next;
-        tmp->next = nullptr;
-        delete tail;
-        tail = tmp;
+        Elem* tmp = head_->next_;
+        delete head_;
+        head_ = tmp;
       }
-      sz--;
-    };
+      sz_--;
+    }
+
+    void push_back(const T& value)
+    {
+      if (empty()) {
+        head_ = new Elem(value);
+        tail_ = head_;
+      } else {
+        tail_->next_ = new Elem(value);
+        tail_ = tail_->next_;
+      }
+      sz_++;
+    }
+
+    void push_back(T&& value)
+    {
+      if (empty()) {
+        head_ = new Elem(std::move(value));
+        tail_ = head_;
+      } else {
+        tail_->next_ = new Elem(std::move(value));
+        tail_ = tail_->next_;
+      }
+      sz_++;
+    }
+
+    void pop_back()
+    {
+      if (empty()) {
+        return;
+      }
+      if (head_ == tail_) {
+        delete head_;
+        head_ = nullptr;
+        tail_ = nullptr;
+      } else {
+        Elem* tmp = head_;
+        while (tmp->next_ != tail_) {
+          tmp = tmp->next_;
+        }
+        tmp->next_ = nullptr;
+        delete tail_;
+        tail_ = tmp;
+      }
+      sz_--;
+    }
 
     Iter< T > insert_after(Iter< T > pos, const T& value);
     Iter< T > insert_after(Iter< T > pos, T&& value);
     Iter< T > erase_after(Iter< T > pos);
 
-    void clear() {
-      while (head != nullptr) pop_front();
-    };
+    void clear()
+    {
+      while (head_ != nullptr) {
+        pop_front();
+      }
+    }
   };
 
   template < class T >
-  inline void sum(T& a, const T& b) {
+  void sum(T& a, const T& b)
+  {
     if (std::numeric_limits<T>::max() - b < a) {
       throw std::overflow_error("Overflow");
     }
